79fb5b1c4a7efefebc728e09907d1165b13138a7
daviz7 <davis_zxt@163.com>
2018-09-05 15:33:22 +0800

    init

---
+++ src/cn/itcast/web/HelloServlet.java
@@ -0,0 +1,17 @@
+package cn.itcast.web;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().print("hello servlet");
+
+        Gson gson = new Gson();
+    }
+}
